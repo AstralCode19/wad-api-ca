ddc2742495a007f2023be2ca5bed8d9d7e4970df
Laisha <ljordan1813@gmail.com>
2025-05-25 20:42:18 +0100

    Completed authenticationContext.jsx

--- react-movies/src/contexts/authenticationContext.jsx
+++ react-movies/src/contexts/authenticationContext.jsx
@@ -2,29 +2,54 @@ import {useState, createContext} from "react";
 import { accountLogin } from "../api/tmdb-api";
 import { accountSignup } from "../api/tmdb-api";
 
-// export const AuthenticationContext = createContext(null);
+export const AuthenticationContext = createContext(null); //eslint-disable-line
 
-// const authenticationContext = (props) => {
+const authenticationContextProvider = (props) => {
+    const existingToken = localStorage.getItem("token");
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [authToken, setAuthToken] = useState(existingToken); //eslint-disable-line
+    const [userName, setUserName] = useState("");
 
-//     const authenticate = async (username, password) => {
-//         const result = await accountLogin (username, password);
-//         if (result.token) {
-//             setToken(result.token)
-//             setIsAuthenticated(true);
-//             setUserName(username);
-//         }
-//     };
+    //Setting a JWT Token
+    const setToken = (data) => {
+        localStorage.setItem("token", data);
+        setAuthToken(data);
+    }
 
-//     return (
-//         <authenticationContext.Provider
-//             value={{
-//                 authenticate,
-//                 username
-//             }}
-//         >
-//             {props.children}
-//         </authenticationContext.Provider>
-//     )
-// };
+    // Making sure it is the correct user logging in
+    const authenticate = async (username, password) => {
+        const result = await accountLogin (username, password);
+        if (result.token) {
+            setToken(result.token)
+            setIsAuthenticated(true);
+            setUserName(username);
+        }
+    };
 
-// export default authenticationContext;
+    // Signup
+    const register = async (username, password) => {
+        const result = await accountSignup(username, password);
+        return result.success;
+    };
+
+    // Signout
+    const signout = () => {
+        setTimeout(() => setIsAuthenticated(false), 150);
+    }
+
+    return (
+        <AuthenticationContext.Provider
+            value={{
+                isAuthenticated,
+                authenticate,
+                register,
+                signout,
+                userName
+            }}
+        >
+            {props.children} {/*eslint-disable-line*/}
+        </AuthenticationContext.Provider>
+    );
+};
+
+export default authenticationContextProvider;
